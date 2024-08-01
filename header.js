f6866cbede8921f55bcc59c66f587b84f0ae8ff4
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-01 17:14:28 -0500

    Update and rename header.jsx to header.js

---
+++ header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../PortfolioStyles.css'; // Adjust the path if necessary
+
+const Header = () => {
+  return (
+    <header>
+      <h1>John Doe</h1> {/* Fixed closing tag for h1 */}
+      <p>Web Developer & Designer</p>
+    </header>
+  );
+};
+
+export default Header;
