ad639730f5ae61a1d580292f200485e39f12d8b7
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-01 16:53:06 -0500

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,14 @@
+// src/Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <h2>Contact</h2>
+      <p>Email: example@example.com</p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/yourprofile">Your Profile</a></p>
+    </section>
+  );
+};
+
+export default Contact;
