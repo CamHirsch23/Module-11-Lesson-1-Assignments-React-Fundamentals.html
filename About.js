46c2b42f35f927398a7f85cfa61962928b451b80
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-01 17:15:22 -0500

    Update About.js

--- About.js
+++ About.js
@@ -1,13 +1,15 @@
-// src/About.js
 import React from 'react';
+import '../PortfolioStyles.css'; // Adjust the path if necessary
 
-const About = () => {
-  return (
-    <section className="about">
-      <h2>About Me</h2>
-      <p>This is a brief description about yourself or a fictional character.</p>
-    </section>
-  );
-};
+class About extends React.Component {
+  render() {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <p>I am a passionate Web Developer with a love for creating dynamic and user-friendly websites. Welcome to my portfolio!</p>
+      </section>
+    );
+  }
+}
 
 export default About;
