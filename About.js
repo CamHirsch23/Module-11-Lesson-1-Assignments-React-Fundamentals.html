8247e2c7b867bdf646d6337c0189211fbcd92d96
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-01 16:52:03 -0500

    Create About.js

---
+++ About.js
@@ -0,0 +1,13 @@
+// src/About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about yourself or a fictional character.</p>
+    </section>
+  );
+};
+
+export default About;
