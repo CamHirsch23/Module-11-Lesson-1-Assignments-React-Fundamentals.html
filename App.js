eb72c63da138c99b1066531ab9b48c59d6692b43
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-04 20:48:40 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Contact from './components/Contact';
-import './PortfolioStyles.css'; // Assuming this is the correct path for your CSS file
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Skills from './Skills';
+import Contact from './Contact';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <About />
-      <Contact />
-    </div>
-  );
-};
+function App() {
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                    <Route path="/projects" component={Projects} />
+                    <Route path="/skills" component={Skills} />
+                    <Route path="/contact" component={Contact} />
+                </Switch>
+            </div>
+        </Router>
+    );
+}
 
 export default App;
